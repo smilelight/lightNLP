33760626b8b1d9d085d5791015c1199bde60b5cc
lightsmile <iamlightsmile@gmail.com>
2019-02-05 13:53:38 +0800

    change some file

--- test/test_sa.py
+++ test/test_sa.py
@@ -1,4 +1,4 @@
-from lightnlp.sl import SA
+from lightnlp.tc import SA
 
 sa_model = SA()
 
@@ -6,12 +6,12 @@ train_path = '/home/lightsmile/Projects/NLP/chinese_text_cnn/data/train.sample.t
 dev_path = '/home/lightsmile/Projects/NLP/chinese_text_cnn/data/dev.sample.tsv'
 vec_path = '/home/lightsmile/Downloads/1410356697_浅笑哥fight/自然语言处理/词向量/sgns.zhihu.bigram-char'
 
-# sa_model.train(train_path, vectors_path=vec_path, dev_path=dev_path, save_path='./sa_saves')
+sa_model.train(train_path, vectors_path=vec_path, dev_path=dev_path, save_path='./sa_saves')
 
-sa_model.load()
+# sa_model.load('sa_saves')
 
-from pprint import pprint
+# from pprint import pprint
 
-sa_model.test(train_path)
+# sa_model.test(train_path)
 
-# pprint(sa_model.predict('另一个很酷的事情是，通过框架我们可以停止并在稍后恢复训练。'))
+# pprint(sa_model.predict('外观漂亮，安全性佳，动力够强，油耗够低。'))
